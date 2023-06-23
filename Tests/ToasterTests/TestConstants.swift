3d36854d647b790c58776d73f83091261666c80c
Stanislav Kirichok <kirichok.stas@gmail.com>
2023-06-23 17:49:06 -0400

    Added tests

---
+++ Tests/ToasterTests/TestConstants.swift
@@ -0,0 +1,14 @@
+//
+//  TestConstants.swift
+//  ToasterTests
+//
+//  Created by Stas Kirichok on 23.06.2023.
+//
+
+import Foundation
+
+enum TestConstants {
+    static let oneSecondDelay = TimeInterval(1)
+    static let twoSecondsDelay = TimeInterval(2)
+    static let tolerance: TimeInterval = 0.2
+}
---
+++ Tests/ToasterTests/ToastSchedulerTests.swift
@@ -0,0 +1,91 @@
+//
+//  ToastSchedulerTests.swift
+//  ToasterTests
+//
+//  Created by Stas Kirichok on 23.06.2023.
+//
+
+import XCTest
+@testable import Toaster
+import Combine
+import SwiftUI
+
+
+
+final class ToastSchedulerTests: XCTestCase {
+    private var scheduler = ToastScheduler()
+    private let predefinedToast = Toast(message: "Predefined", style: .error, dismissDelay: TestConstants.oneSecondDelay)
+    private var cancellables = Set<AnyCancellable>()
+
+    override func setUpWithError() throws {
+        scheduler = ToastScheduler()
+    }
+
+    override func tearDownWithError() throws {
+        cancellables.removeAll()
+    }
+
+    func testCurrentTestIsNilOnStart() throws {
+        XCTAssertNil(scheduler.currentToast)
+    }
+
+    func testAddingPredefinedToast() throws {
+        let expectation = expectation(description: "Toast should be removed after 1 second")
+
+        var counter = 0
+        scheduler.$currentToast
+            .sink { wrapper in
+                switch counter {
+                case 1:
+                    if case .predefinedView(let toast) = wrapper {
+                        XCTAssertEqual(toast, self.predefinedToast)
+                    } else {
+                        XCTFail("Toast wasn't appeared")
+                    }
+                case 2:
+                    if wrapper == nil {
+                        expectation.fulfill()
+                    }
+                default:
+                    break
+                }
+
+                counter += 1
+            }
+            .store(in: &cancellables)
+
+        scheduler.present(predefinedToast)
+
+        wait(for: [expectation], timeout: TestConstants.oneSecondDelay + TestConstants.tolerance)
+    }
+
+    func testAddingCustomToast() throws {
+        let expectation = expectation(description: "Toast should be removed after 2 second")
+
+        var counter = 0
+        scheduler.$currentToast
+            .sink { wrapper in
+                switch counter {
+                case 1:
+                    if case .customView(let customToast) = wrapper {
+                        XCTAssertEqual(customToast.dismissDelay, TestConstants.twoSecondsDelay)
+                    } else {
+                        XCTFail("Toast wasn't appeared")
+                    }
+                case 2:
+                    if wrapper == nil {
+                        expectation.fulfill()
+                    }
+                default:
+                    break
+                }
+
+                counter += 1
+            }
+            .store(in: &cancellables)
+
+        scheduler.present(EmptyView(), dismissDelay: TestConstants.twoSecondsDelay)
+
+        wait(for: [expectation], timeout: TestConstants.twoSecondsDelay + TestConstants.tolerance)
+    }
+}
